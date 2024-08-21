d5d1b4e6e82f3d3f659690bace825c595e64b5e7
Chukwuemeka Patrick <79291626+djemmydon@users.noreply.github.com>
2024-08-21 07:22:51 +0100

    added cuisine

--- server/index.ts
+++ server/index.ts
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000", "https://buka-store.vercel.app"],
+    origin: ["http://localhost:3000", "https://buka-store-client.vercel.app"],
   })
 );
 
