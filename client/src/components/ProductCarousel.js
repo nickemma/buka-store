4c5b8ec2c6b8604ebfc60193ac9c19480ee48db4
Chukwuemeka Patrick <79291626+djemmydon@users.noreply.github.com>
2024-08-23 02:48:42 +0100

    added new page

--- client/src/components/ProductCarousel.js
+++ client/src/components/ProductCarousel.js
@@ -19,39 +19,42 @@ export default function ProductCarousel({ header, item }) {
   // const { handleAddToCart, handleDecrement, handleIncrement, cart } = useCart(data);
   return (
     <div className="w-full  mx-auto  pt-10 ">
-      <h2 className="text-[1.2rem] mt-3 font-bold">{header}</h2>
+      <div className="flex justify-between items-center mt-4">
+        <h2 className="text-[1rem] md:text-[1.2rem] mt-3 font-bold">{header}</h2>
+        <Link className="text-primary" href={`/cuisine`}>View all</Link>
+      </div>
       <Carousel className="w-full">
         <CarouselContent>
-        <CarouselItem
-              key={item?._id}
-              className="basis-1/1 md:basis-1/3 lg:basis-1/4"
-            >
-              <div className="p-4">
-                <Card>
-                  <img
-                    src={"/food2-buka.png"}
-                    alt={item?.title}
-                    width={300}
-                    height={300}
-                    className="aspect-square object-cover rounded-md"
-                  />
-                  <CardContent className="p-4">
-                    <h3 className="font-semibold text-[1rem]">
-                      {item?.cuisine_name}
-                    </h3>
-                    <h3 className="font-semibold text-[0.6rem]">
-                      {item?.cuisine_owner?.buka_name}
-                    </h3>
-                    <div className="text-primary font-semibold">
-                      {item?.price}
-                    </div>
-                    <Link href={`/kitchen/${item?.cuisine_owner?._id}`}>
-                      <Button>View Cuisine</Button>
-                    </Link>
-                  </CardContent>
-                </Card>
-              </div>
-            </CarouselItem>
+          <CarouselItem
+            key={item?._id}
+            className="basis-1/1 md:basis-1/3 lg:basis-1/4"
+          >
+            <div className="p-4">
+              <Card>
+                <img
+                  src={"/food2-buka.png"}
+                  alt={item?.title}
+                  width={300}
+                  height={300}
+                  className="aspect-square object-cover rounded-md"
+                />
+                <CardContent className="p-4">
+                  <h3 className="font-semibold text-[1rem]">
+                    {item?.cuisine_name}
+                  </h3>
+                  <h3 className="font-semibold text-[0.6rem]">
+                    {item?.cuisine_owner?.buka_name}
+                  </h3>
+                  <div className="text-primary font-semibold">
+                    {item?.price}
+                  </div>
+                  <Link href={`/kitchen/${item?.cuisine_owner?._id}`}>
+                    <Button>View Cuisine</Button>
+                  </Link>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
         </CarouselContent>
       </Carousel>
     </div>
