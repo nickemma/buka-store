bfc9566d699aa7e00d33c6eb300d0da5f5bf543b
Chukwuemeka Patrick <79291626+djemmydon@users.noreply.github.com>
2024-08-26 12:53:02 +0100

    added buka

--- client/src/app/api/create-payment-intent/route.js
+++ client/src/app/api/create-payment-intent/route.js
@@ -1,11 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
-const stripe = require("stripe")("sk_test_51Pr8CxHlirJCHboKHw939YIAxgOS1pH1dJ7OTdS2edvYE27HnNV21nR9oNvogw5rY5GMRH6yRtfgSLJy0HHjUBQ800I2rurMwc");
+import stripe from "stripe"
 
+const stripeIntent = stripe("sk_test_51Pr8CxHlirJCHboKHw939YIAxgOS1pH1dJ7OTdS2edvYE27HnNV21nR9oNvogw5rY5GMRH6yRtfgSLJy0HHjUBQ800I2rurMwc")
 export async function POST(request) {
+
+  
   try {
     const { amount } = await request.json();
 
-    const paymentIntent = await stripe.paymentIntents.create({
+    const paymentIntent = await stripeIntent.paymentIntents.create({
       amount: amount * 100,
       currency: "usd",
       automatic_payment_methods: { enabled: true },
