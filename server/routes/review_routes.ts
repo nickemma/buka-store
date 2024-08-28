3d56671bb3eccde0a6f76514f768255d6b09b327
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2024-08-28 16:34:10 +0100

    Added a get review route

--- server/routes/review_routes.ts
+++ server/routes/review_routes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { protect } from '../middleware/auth';
-import { createReview } from '../handlers/review_handler';
+import { createReview, getReviews } from '../handlers/review_handler';
 
 const router = express.Router();
 
-router.post('/', protect, createReview);
+router.post('/', protect, createReview );
+router.get('/', getReviews );
 
 export default router;
